2a8f301fd31f19c6a90a5976c28c42c24ee2ca6b
Michael Fausnaugh <faus@mit.edu>
2022-12-01 10:48:08 -0500

    update TNS through s59, added velocity, pointing, times through s59

--- TNS/TNS_scrapper.py
+++ TNS/TNS_scrapper.py
@@ -144,7 +144,8 @@ def get_file(url):                                                   #
 #active_sectors = np.r_[49,50,51]
 
 #starting in S52, modified to only pull transients from within the last 3 months of sector start
-active_sectors = np.r_[57:58]
+#active_sectors = np.r_[55:60]
+active_sectors = [58,59]
 
 #these are imported from catalog2tess_px/camera_pointings/cam_pointings.py
 cams = [cam_pointings.cam1, 
@@ -155,7 +156,7 @@ cams = [cam_pointings.cam1,
 for s in active_sectors:
     for ii,cam in enumerate(cams):
         #pick out individual cameras
-        #if ii < 2:
+        #if ii < 1:
         #    continue
         sleep(3)
         print('searching sector {}, camera {}'.format(s, ii+1))
