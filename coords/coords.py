7b03c2e5f30c8469bc12e75820e2a88ab253170b
Michael Fausnaugh <faus@mit.edu>
2024-05-28 06:48:37 -0500

    removed unix style path from coords.py

--- coords/coords.py
+++ coords/coords.py
@@ -15,7 +15,7 @@ def aber_stars_and_starspx(ra,dec,mag,
     aber_stars = os.path.join(path_use,'aber_stars1')
     starspx    =  os.path.join(path_use,'starspx8')
     
-    geom_path = os.path.join(path_use,'../coords/')
+    geom_path = os.path.join(path_use,'..','coords')
     
     data_string = '\n'.join([' '.join(r.astype(str)) for r in np.c_[ra,dec,mag] ])
     p1 = Popen([aber_stars, "1",str(v1), str(v2), str(v3)],
